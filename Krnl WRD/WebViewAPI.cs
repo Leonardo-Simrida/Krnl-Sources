c09a026e4daf2dfbed32489f7b208c1d6e2fa55f
Leonardo Briley <leonardobriley09@gmail.com>
2024-12-29 20:21:22 -0700

    Added MonacoEditor now working on styles and AvalonEdit / MonacoEditor switch

---
+++ Krnl WRD/WebViewAPI.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Krnl_WRD
+{
+    public class WebViewAPI : WebView2
+    {
+        public bool isDOMLoaded { get; set; } = false;
+        private string ToSetText;
+        private string LatestRecievedText;
+
+        public event EventHandler EditorReady;
+
+        public WebViewAPI()
+        {
+            this.Source = new Uri($"{Directory.GetCurrentDirectory()}\\bin\\monaco\\monaco.html");
+            this.DefaultBackgroundColor = System.Drawing.Color.FromArgb(255, 25, 25, 25);
+            this.CoreWebView2InitializationCompleted += WebViewAPI_CoreWebView2InitializationCompleted;
+        }
+
+        protected virtual void OnEditorReady()
+        {
+            EventHandler handler = EditorReady;
+            if (handler != null)
+                handler(this, new EventArgs());
+        }
+
+        private void WebViewAPI_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            this.CoreWebView2.DOMContentLoaded += CoreWebView2_DOMContentLoaded;
+            this.CoreWebView2.WebMessageReceived += CoreWebView2_WebMessageReceived;
+            this.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            this.CoreWebView2.Settings.AreDevToolsEnabled = false;
+        }
+
+        private void CoreWebView2_WebMessageReceived(object sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs e) => LatestRecievedText = e.TryGetWebMessageAsString();
+
+        private async void CoreWebView2_DOMContentLoaded(object sender, Microsoft.Web.WebView2.Core.CoreWebView2DOMContentLoadedEventArgs e)
+        {
+            await Task.Delay(1000);
+            isDOMLoaded = true;
+            SetText(ToSetText);
+            OnEditorReady();
+        }
+
+        public async Task<string> GetText()
+        {
+            if (isDOMLoaded)
+            {
+                await this.ExecuteScriptAsync("window.chrome.webview.postMessage(editor.getValue())");
+                await Task.Delay(200);
+
+                return LatestRecievedText;
+            }
+            return string.Empty;
+        }
+
+        public async void SetText(string text)
+        {
+            if (isDOMLoaded)
+                await CoreWebView2.ExecuteScriptAsync($"SetText(\"{HttpUtility.JavaScriptStringEncode(text)}\")");
+        }
+
+        public void AddIntellisense(string label, Types type, string description, string insert)
+        {
+            if (isDOMLoaded)
+            {
+                string selectedType = type.ToString();
+                if (type == Types.None)
+                    selectedType = "";
+                this.ExecuteScriptAsync($"AddIntellisense({label}, {selectedType}, {description}, {insert});");
+            }
+        }
+
+        public void Refresh()
+        {
+            if (isDOMLoaded)
+                this.ExecuteScriptAsync($"Refresh();");
+        }
+    }
+
+    public enum Types
+    {
+        Class,
+        Color,
+        Constructor,
+        Enum,
+        Field,
+        File,
+        Folder,
+        Function,
+        Interface,
+        Keyword,
+        Method,
+        Module,
+        Property,
+        Reference,
+        Snippet,
+        Text,
+        Unit,
+        Value,
+        Variable,
+        None
+    }
+}
--- Krnl WRD/Windows/MainWindow.xaml.cs
+++ Krnl WRD/Windows/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,19 +16,45 @@ using System.Windows.Shapes;
 
 namespace Krnl_WRD
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public int TabCount { get; set; } = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private WebViewAPI GetViewAPI()
+        {
+            return Tabs.SelectedContent as WebViewAPI;
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            MessageBoxResult messageBoxResult = MessageBox.Show("Hello, User by clicking (YES) you are accepting the fact that a false positive will show up when scaning the application. However if you don't want to see a false positive when you scaning Krnl click (NO) to exit the app. Thanks!", "Warning", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
+            if (messageBoxResult == MessageBoxResult.Yes) { }
+            else if (messageBoxResult == MessageBoxResult.No)
+            {
+                Application.Current.Shutdown();
+            }
+
+            AddingStartTab();
+        }
+
+        private void AddingStartTab()
+        {
+            WebViewAPI webViewAPI = new WebViewAPI();
+            TabItem tabItem = new TabItem()
+            {
+                Header = "Script " + TabCount + ".lua" as string,
+                Height = 15,
+                Content = webViewAPI
+            };
+            Tabs.Items.Add(tabItem);
+            Tabs.SelectedItem = tabItem;
+            TabCount++;
         }
 
         private void Topbar_MouseDown(object sender, MouseButtonEventArgs e)
